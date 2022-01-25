7a63f4bd4d5f048b2a3bf4dfa771f32cb0fd50f4
Yoodahun <dahun4032@gmail.com>
2022-01-25 23:21:54 +0900

    ADD then scenario

--- Section10/tests/acceptance/steps/navigation.py
+++ Section10/tests/acceptance/steps/navigation.py
@@ -6,11 +6,12 @@ use_step_matcher('re')
 
 @given('I am on the homepage')
 def step_impl(context):
-    browser = webdriver.Chrome("tests/driver/chromedriver")
-    browser.get('http://127.0.0.1:5000')
-    #
-    # browser.close()
-    # browser.quit()
+    context.browser = webdriver.Chrome("tests/driver/chromedriver")
+    context.browser.get('http://127.0.0.1:5000')
 
 
+@then("I am on the blog page")
+def step_impl(context):
+    expected_url = "http://127.0.0.1:5000/blog"
+    assert context.browser.current_url == expected_url
 
