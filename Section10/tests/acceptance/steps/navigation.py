2a10ab779bf36999499f3f63fc3ba1a47a26653e
Yoodahun <dahun4032@gmail.com>
2022-01-26 23:47:22 +0900

    ADD more Scenarios

--- Section10/tests/acceptance/steps/navigation.py
+++ Section10/tests/acceptance/steps/navigation.py
@@ -15,3 +15,14 @@ def step_impl(context):
     expected_url = "http://127.0.0.1:5000/blog"
     assert context.browser.current_url == expected_url
 
+
+@given("I am on the blog page")
+def step_impl(context):
+    context.browser = webdriver.Chrome("tests/driver/chromedriver")
+    context.browser.get('http://127.0.0.1:5000/blog')
+
+
+@then("I am on the homepage")
+def step_impl(context):
+    expected_url = "http://127.0.0.1:5000/"
+    assert context.browser.current_url == expected_url
