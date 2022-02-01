9bacb834f2fc1d72e3c6c2fbfb5a9d7a4f5c22db
Yoodahun <dahun4032@gmail.com>
2022-02-01 14:57:29 +0900

    ADD HomePage locators and models UPDATE content.py logic using locator and model class

---
+++ Section10/tests/acceptance/page_model/homepage.py
@@ -0,0 +1,17 @@
+from tests.acceptance.locators.homepage import HomePageLocators
+
+
+class HomePage:
+    def __init__(self, driver):
+        self.driver = driver
+
+    def url(self):
+        return 'http://127.0.0.1:5000/'
+
+    def title(self):
+        return  self.driver.find_element(*HomePageLocators.TITLE)
+
+    def blog_link(self):
+        return self.driver.find_element(*HomePageLocators.NAVIGATION_LINK)
+
+
--- Section10/tests/acceptance/steps/content.py
+++ Section10/tests/acceptance/steps/content.py
@@ -1,5 +1,6 @@
 from behave import *
-from selenium.webdriver.common.by import By
+from tests.acceptance.locators.homepage import HomePageLocators
+from tests.acceptance.page_model.homepage import HomePage
 
 use_step_matcher('re')
 
@@ -7,12 +8,13 @@ use_step_matcher('re')
 
 @then("There is a title shown on the page")
 def step_impl(context):
-    title_tag = context.browser.find_element(By.TAG_NAME, 'h1')
-    assert title_tag.is_displayed()
+    page = HomePage(context.browser)
+    assert page.title().is_displayed()
 
 
 @step('The title tag has content "(.*)"')
 def step_impl(context, content):
-    title_tag = context.browser.find_element(By.TAG_NAME, 'h1')
-    assert title_tag.text == content
+    page = HomePage(context.browser)
+
+    assert page.title().text == content
 
