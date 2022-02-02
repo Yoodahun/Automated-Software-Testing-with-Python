3e709503e3eee89f2ac84ce1839d2826b9cce728
Yoodahun <dahun4032@gmail.com>
2022-02-02 15:22:18 +0900

    UPDATE refactoring method

--- Section10/tests/acceptance/steps/content.py
+++ Section10/tests/acceptance/steps/content.py
@@ -5,16 +5,14 @@ from tests.acceptance.page_model.homepage import HomePage
 use_step_matcher('re')
 
 
-
 @then("There is a title shown on the page")
 def step_impl(context):
     page = HomePage(context.browser)
-    assert page.title().is_displayed()
+    assert page.title.is_displayed()
 
 
 @step('The title tag has content "(.*)"')
 def step_impl(context, content):
     page = HomePage(context.browser)
 
-    assert page.title().text == content
-
+    assert page.title.text == content
