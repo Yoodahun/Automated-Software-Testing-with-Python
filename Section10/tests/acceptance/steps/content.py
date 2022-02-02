c2c782fe1bbb9c8cf338944a87c6b14256bf72e8
Yoodahun <dahun4032@gmail.com>
2022-02-02 15:38:13 +0900

    UPDATE refactoring variable name

--- Section10/tests/acceptance/steps/content.py
+++ Section10/tests/acceptance/steps/content.py
@@ -1,5 +1,5 @@
 from behave import *
-from tests.acceptance.locators.homepage import HomePageLocators
+from tests.acceptance.page_model.base_page import BasePage
 from tests.acceptance.page_model.homepage import HomePage
 
 use_step_matcher('re')
@@ -7,12 +7,12 @@ use_step_matcher('re')
 
 @then("There is a title shown on the page")
 def step_impl(context):
-    page = HomePage(context.browser)
+    page = BasePage(context.browser)
     assert page.title.is_displayed()
 
 
 @step('The title tag has content "(.*)"')
 def step_impl(context, content):
-    page = HomePage(context.browser)
+    page = BasePage(context.browser)
 
     assert page.title.text == content
